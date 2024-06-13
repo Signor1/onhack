bbcd57f0695e464c53c3b49d836ff268127238a9
signor1 <emmanuelomemgboji@gmail.com>
2024-06-13 13:14:27 +0100

    feat: project setup

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import MaxWrapper from "@/components/shared/MaxWrapper";
+
+
+export default function Home() {
+  return (
+
+    <MaxWrapper>
+      <main className="w-full">
+      </main>
+    </MaxWrapper>
+
+  )
+}
---
+++ src/components/shared/MaxWrapper.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+
+import { cn } from "@/lib/utils";
+
+type MaxWrapperTypes = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const MaxWrapper: FC<MaxWrapperTypes> = ({ children, className }) => {
+  return (
+    <div className={cn("w-full mx-auto max-w-[1368px] px-4", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default MaxWrapper;
