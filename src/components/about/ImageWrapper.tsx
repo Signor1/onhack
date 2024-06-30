df3d91c6e9a7810307ae21b46cc738150f5f663c
signor1 <emmanuelomemgboji@gmail.com>
2024-06-30 12:19:00 +0100

    feat: updated favicon && week3 image

--- src/components/about/ImageWrapper.tsx
+++ src/components/about/ImageWrapper.tsx
@@ -8,7 +8,7 @@ const ImageWrapper = ({ image, shapeone, shapetwo }: {
 }) => {
     return (
         <div className='relative w-full h-full'>
-            <Image src={image} alt='Image' className='w-full md:rounded-[36px] rounded-[20px] h-full object-cover' width={561} height={665} quality={100} priority />
+            <Image src={image} alt='Image' className='w-full md:rounded-[36px] rounded-[20px] h-full object-top md:object-center object-cover' width={561} height={665} quality={100} priority />
 
             <div className="absolute md:-right-3 md:-top-4 -right-4 -top-3 md:w-[96px] md:h-[96px] w-[50px] h-[50px]">
                 <Image src={shapeone} alt="shape" className="h-full w-full" width={131} height={131} quality={100} priority />
